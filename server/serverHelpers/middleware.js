7c795027d37e685c38698473b17d7b482a3eede1
Matt Dubie <mbdubie@gmail.com>
2016-07-27 21:48:56 -0700

    resolved middleware issue

--- server/serverHelpers/middleware.js
+++ server/serverHelpers/middleware.js
@@ -1,16 +1,15 @@
-const path = require('path');
 const morgan = require('morgan');
-const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
+const auth = require('./authentication.js');
 
 module.exports = (app) => {
-  app.use(express.static(path.join(__dirname, '../../client')));
   app.use(morgan('dev'));
-  app.use(express.static('public'));
-  app.use(express.cookieParser());
-  app.use(express.bodyParser());
-  app.use(passport.initialize());
-  app.use(app.router);
+  app.use(cookieParser());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }));
+  app.use(auth.passport.initialize());
+  app.use(auth.passport.session());
 };
