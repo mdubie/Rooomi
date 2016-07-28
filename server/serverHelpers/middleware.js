b5079be4049439de9b27a4c509c2c8d6b29f5792
Matt Dubie <mbdubie@gmail.com>
2016-07-27 20:52:43 -0700

    setup for autherntication

--- server/serverHelpers/middleware.js
+++ server/serverHelpers/middleware.js
@@ -1,8 +1,16 @@
 const path = require('path');
 const morgan = require('morgan');
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
 
 module.exports = (app) => {
   app.use(express.static(path.join(__dirname, '../../client')));
   app.use(morgan('dev'));
+  app.use(express.static('public'));
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(passport.initialize());
+  app.use(app.router);
 };
