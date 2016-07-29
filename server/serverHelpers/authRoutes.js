c827e9d37e412efa64a952ea91b629bb2d7ca1a6
Matt Dubie <mbdubie@gmail.com>
2016-07-28 17:33:13 -0700

    auth complete

--- server/serverHelpers/authRoutes.js
+++ server/serverHelpers/authRoutes.js
@@ -1,55 +1,46 @@
 const auth = require('./authentication.js');
 const express = require('express');
 const path = require('path');
-const connectEnsureLogin = require('connect-ensure-login');
 
 const userController = require('../db/user/userController.js');
 
-const isAuthenticated = function (req, res, next) {
-  if (req.isAuthenticated())
-    return next();
+const isAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) { return next(); }
   res.redirect('/');
-}
+};
 
 module.exports = (app) => {
-  app.use('/home', express.static(path.join(__dirname, '../../client/')));
-
-  app.get('/', function(req, res) {
-    if (req.user) {
-      res.redirect('/home');
-    } else {
-      res.redirect('/login');
-    }
+  app.get('/', (req, res) => {
+    const url = req.user ? '/home' : '/login';
+    res.redirect(url);
   });
 
-  app.get('/home', isAuthenticated);
+  app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/login.html'));
+  });
 
-  app.get('/login', function(req, res) {
-    res.sendFile(path.join(__dirname, '../../public/login.html'))
-  })
+  app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/signup.html'));
+  });
 
-  app.get('/signup', function(req, res) {
-    res.sendFile(path.join(__dirname, '../../public/signup.html'))
-  })
+  app.use('/home', isAuthenticated, express.static(path.join(__dirname, '../../client/')));
 
   app.post('/login', auth.passport.authenticate('local', { failureRedirect: '/login', successRedirect: '/home'}));
 
-  app.post('/signup', function(req, res) {
+  app.post('/signup', (req, res) => {
     const userCreds = req.body;
-    userController.signup(userCreds, function(err, data) {
-      if (!err) {
-        if (data) {
-          res.redirect(`/home?username=${data.username}&house=${data.house}`);
-        } else {
-          res.redirect('/signup');
-        }
-      }
+    userController.signup(userCreds, (err, data) => {
+      if (err) { console.log('Signup error ', err); }
+      const url = data ? '/login' : '/signup';
+      res.redirect(url);
     });
   });
 
-  app.post('/signout', function(req, res) {
+  app.post('/signout', (req, res) => {
     req.logout();
     res.redirect('/');
   });
 };
 
+
+        // ? `/home?username=${data.username}&house=${data.house}`
