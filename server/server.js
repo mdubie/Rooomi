e5e0b6c48e8c613a9d46100ac714078145c0ed65
Matt Dubie <mbdubie@gmail.com>
2016-07-28 17:06:06 -0700

    merge

--- server/server.js
+++ server/server.js
@@ -9,11 +9,11 @@ db.connect();
 const middleware = require('./serverHelpers/middleware.js');
 middleware(app);
 
-const socketRoutes = require('./serverHelpers/socketRoutes.js');
-io.on('connection', (socket) => socketRoutes(socket));
-
 const authRoutes = require('./serverHelpers/authRoutes.js');
 authRoutes(app);
+
+const socketRoutes = require('./serverHelpers/socketRoutes.js');
+io.on('connection', (socket) => socketRoutes(socket));
 
 server.listen(3000, (err) => {
   err ? console.log('server error', err) : console.log('server running port 3000');
