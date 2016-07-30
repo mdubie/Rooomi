d25872d8d2b842a3625b138f6336739c8344e2b3
Matt Dubie <mbdubie@gmail.com>
2016-07-30 11:46:42 -0700

    add buttons to only user tasks

--- client/src/components/taskboard/TaskListEntry.js
+++ client/src/components/taskboard/TaskListEntry.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { ListGroupItem } from 'react-bootstrap';
+import { ListGroupItem, Button } from 'react-bootstrap';
 import moment from 'moment';
 
 
 export const TaskListEntry = ({ isUser, username, task, socket }) => {
+  const button = (
+    <Button bsStyle="primary" onClick={() => { if (isUser) { socket.emit('completeTask', task); } }}>
+      Task completed
+    </Button>
+  );
+
+  const render = isUser ? button : '';
+
   return (
-    <ListGroupItem
-      onClick={() => { if (isUser) { socket.emit('completeTask', task); } }}
-    >
-      <h4>{`${task.assignee}'s task`}</h4>
-      <p>{task.description}</p>
-      <p>{`assigned by ${task.assignor} and due ${moment(task.dueAt).fromNow()}`}</p>
+    <ListGroupItem>
+      <div>
+        <h4>{`${task.assignee}'s task`}</h4>
+        <p>{task.description}</p>
+        <p>{`assigned by ${task.assignor} and due ${moment(task.dueAt).fromNow()}`}</p>
+      </div>
+      <div>
+        {render}
+      </div>
     </ListGroupItem>
   );
 };
